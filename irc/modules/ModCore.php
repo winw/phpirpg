dea437af41bb9ddea71156ad5b320dee839bec4d
win <winwarrior@hotmail.com>
2013-02-24 18:18:05 +0100

    Suppression de la classe IrpgUser (pas pratique à utiliser)

--- irc/modules/ModCore.php
+++ irc/modules/ModCore.php
@@ -1,38 +1,4 @@
-<?php
- class IrpgUser {
-  private $iIdUser = null;
-  private $oWho;
-  
-  public function __construct(ParsedMask $oWho) {   
-   $this->oWho = $oWho;
-   $this->refresh();
-  }
-  
-  private function __clone(){}
-  
-  public function refresh() {
-   $oChannelUsers = new dbChannelUsers();
-   if ($oChannelUser = $oChannelUsers->select('id_irpg_user')->where('channel = ? AND nick = ? AND user = ? AND host = ?', IRPG_CHANNEL, $this->oWho->getNick(), $this->oWho->getUser(), $this->oWho->getHost())->fetch()) {
-    if ($oChannelUser->id_irpg_user) {
-     $this->iIdUser = (int)$oChannelUser->id_irpg_user;
-    }
-   }
-  }
-  
-  public function isLogged() {
-   return $this->iIdUser !== null;
-  }
-  
-  public function getCoords() {
-   if ($this->isLogged()) {
-    $oIrpgUsers = new dbIrpgUsers();
-    if ($oIrpgUser = $oIrpgUsers->select('x, y')->where('id = ?', $this->iIdUser)->fetch()) {
-     return array('x' => $oIrpgUser->x, 'y' => $oIrpgUser->y);
-    }
-   }
-  }
- }
- 
+<?php 
  class ModCore extends Module {
   private static function encodePassword($sPassword) {
    return sha1($sPassword.'%'.strrev($sPassword));
@@ -45,14 +11,9 @@
   }
   
   public function onMsg(ParsedMask $oWho, $sTarget, $sMessage){
-   $oCurrentUser = new IrpgUser($oWho);
    $oIrpgUsers = new dbIrpgUsers();
    
-   if ($this->isGameChannel($sTarget)) {
-    if ($oCurrentUser->isLogged()) {
-     //addPenalties($oCurrentUser)
-    }
-   } else if ($this->isMe($sTarget)) {
+   if ($this->isMe($sTarget)) {
     $aTokens = explode(' ', $sMessage);
     $iNbTokens = count($aTokens);
     if ($iNbTokens > 0) {
@@ -60,7 +21,7 @@
       case 'REGISTER':
        if ($iNbTokens != 5) {
         $this->msg($oWho->getNick(), 'Syntax: REGISTER <login> <password> <email> <type>');
-       } else if ($oCurrentUser->isLogged()) {
+       } else if ($this->getUserIdFromMask($oWho)) {
         $this->msg($oWho->getNick(), 'You are already registered and logged');
        } else {
         list(,$sLogin, $sPassword, $sEmail, $sType) = $aTokens;
@@ -94,7 +55,7 @@
        }
       break;
       case 'LOGIN':
-       if ($oCurrentUser->isLogged()) {
+       if ($this->getUserIdFromMask($oWho)) {
         $this->msg($oWho->getNick(), 'You are already logged');
        } else if ($iNbTokens != 3) {
         $this->msg($oWho->getNick(), 'Syntax: LOGIN <login> <password>');
