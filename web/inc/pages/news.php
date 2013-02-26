9e024770fdb6cf7a4ddc08af225888a93c046d99
Chris <z3ckz@hotmail.com>
2013-02-27 00:34:56 +0100

    Optimisation css

--- web/inc/pages/news.php
+++ web/inc/pages/news.php
@@ -1,4 +1,4 @@
 <?php $aoDatas = $oPdo->query('SELECT * FROM `irpg_news` ORDER BY id DESC')->fetchAll(PDO::FETCH_OBJ); foreach ($aoDatas as $oData) { ?>
-<div class="title"><strong>&#8250; <?php echo $oData->title; ?></strong><span class="menuright"><i><?php echo date('j F Y',$oData->datetime); ?></i></span></div>
+<div class="title">&#8250; <?php echo $oData->title; ?><span class="menuright"><?php echo date('j F Y',$oData->datetime); ?></span></div>
 <div class="contents"><?php echo $oData->contents; ?></div>
 <br /><?php } ?>
--- web/inc/pages/rank.php
+++ web/inc/pages/rank.php
@@ -1,11 +1,11 @@
 <div class="table">
 	<div class="tbody">
         <div class="tr_menu">
-        <div class="id"><strong>#</strong></div>
-        <div class="player"><strong>Player</strong></div>
-        <div class="lvl"><strong>Level</strong></div>
-        <div class="time"><strong>Time to level</strong></div>
-        <div class="item"><strong>Itemsum</strong></div>
+        <div class="id">#</div>
+        <div class="player">Player</div>
+        <div class="lvl">Level</div>
+        <div class="time">Time to level</div>
+        <div class="item">Itemsum</div>
         </div>
 		<?php $aoDatas = $oPdo->query('SELECT * FROM `irpg_users` ORDER BY level DESC, time_to_level ASC')->fetchAll(PDO::FETCH_OBJ); foreach ($aoDatas as $oData) { ?><div class="tr">
         <div class="id"><span class="num"></span></div>
