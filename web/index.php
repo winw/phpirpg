87dadaf5721a65762290d14715c5dc6098556732
Chris <z3ckz@hotmail.com>
2013-03-01 19:11:19 +0100

    Ajout de la page count.php et securityPollution à l'include

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
-<?php error_reporting(E_ALL & ~E_WARNING);
+<?php
+error_reporting(E_ALL & ~E_WARNING);
  define('BASE_PATH', dirname(__FILE__).'/');
  require_once 'inc/mysqlman/dbDontEscapeString.class.php';
  require_once 'inc/mysqlman/dbInstance.class.php';
@@ -8,6 +9,10 @@ dbClassGen::$sPath = BASE_PATH.'inc/mysqlman/db/';
  require_once 'inc/mysqlman/dbObject.class.php';
  require_once 'inc/mysqlman/Utils.class.php';
  require_once 'inc/mysqlman/dbPrimitiveObject.class.php';
+ require_once('inc/mysqlman/security_requestPollution.class.php');
+ require_once 'inc/pages/count.php';
+ if (PHP_VERSION < 5.2) trigger_error('Le systeme a besoin de php 5.2 minimum', E_USER_ERROR);
+ if (!class_exists('PDO')) trigger_error('Le systeme a besoin de l\'extension PDO pour gerer les bases de donnees', E_USER_ERROR);
  
  $oPdo = new PDO('mysql:dbname=phpirpg;host=localhost', 'root', '', Array(
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -40,22 +45,28 @@ dbClassGen::$sPath = BASE_PATH.'inc/mysqlman/db/';
 <title>#idle-rpg @ Quakenet.org !</title>
 </head>
 <body>
-<div class="header">
-<a href="?q=news"><img src="inc/pict/header.png" alt="" /></a></div>
-<div class="menu"><a href="?q=news">News</a> • <a href="?q=map">World Map</a> • <a href="?q=rank">Rank</a> • <a href="?q=stats">Statistics</a> • <a href="?q=story">Story</a> • <a href="?q=dev">Development</a> • <a href="?q=faq">FAQ</a> <span class="menuright"><?php $oChannelUsers = new dbChannelUsers(); $iNb = $oChannelUsers->select('COUNT(1) as nb')->where('id_irpg_user IS NOT NULL')->fetch()->nb; { echo '<strong>'.$iNb.'</strong> player'.(($iNb > 1) ? 's' : ''); } ?> connected. <strong><a href="irc://irc.quakenet.org/idle-rpg">Join us!</a></strong></span></div>
+<div class="header"><a href="?q=news"><img src="inc/pict/header.png" alt="" /></a></div>
+<div class="all">
+<div class="menu"><a href="?q=news">News</a> • <a href="?q=map">World Map</a> • <a href="?q=ranking">Ranking</a> • <a href="?q=stats">Statistics</a> <!--• <a href="?q=story">Story</a> -->• <a href="?q=changelog">Changelog</a> • <a href="?q=faq">FAQ</a> <span class="menuright"><?php $oChannelUsers = new dbChannelUsers(); $iNb = $oChannelUsers->select('COUNT(1) as nb')->where('id_irpg_user IS NOT NULL')->fetch()->nb; { echo '<strong>'.$iNb.'</strong> idler'.(($iNb > 1) ? 's' : ''); } ?> connected. <strong><a href="irc://irc.quakenet.org/idle-rpg">Join us!</a></strong></span></div>
 <div class="global"><?php
+if ($aVars = security_requestPollution::check(security_requestPollution::MODE_GET)) {
+trigger_error('Pollution dans la requête : '.implode(', ', $aVars), E_USER_ERROR);
+}
 if (!isset($_GET['q'])) $_GET['q'] = 'news';
+ob_start();
 switch ($_GET['q']) {
 case 'news': include_once('inc/pages/news.php'); break;	
 case 'map':	include_once('inc/pages/map.php'); break;
-case 'rank': include_once('inc/pages/rank.php'); break;
+case 'ranking': include_once('inc/pages/ranking.php'); break;
 case 'stats': include_once('inc/pages/stats.php'); break;
-case 'dev': include_once('inc/pages/dev.php'); break;
+case 'changelog': include_once('inc/pages/changelog.php'); break;
 case 'story': include_once('inc/pages/story.php'); break;
 case 'faq':	include_once('inc/pages/faq.php'); break;
-default: header('Status: 404 Not Found', false, 404);
+default: include_once('inc/pages/404.php');
 }
+ob_end_flush();
 ?></div>
 <div class="footer"><strong>©</strong> idle-rpg created by <a href="mailto:&#119;&#105;&#110;&#064;&#119;&#097;&#114;&#114;&#105;&#111;&#114;&#104;&#111;&#117;&#115;&#101;&#046;&#110;&#101;&#116;">win</a> and <a href="mailto:&#115;&#104;&#105;&#119;&#097;&#110;&#103;&#064;&#111;&#114;&#097;&#110;&#103;&#101;&#046;&#102;&#114;">Shiwang</a> • All rights reserved • Since 2013. </div>
+</div>
 </body>
 </html>
