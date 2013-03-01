58759de31b972aa1294c661302945f75bfca304d
Chris <z3ckz@hotmail.com>
2013-03-01 19:08:10 +0100

    Affichage des news + Pagination

--- web/inc/pages/news.php
+++ web/inc/pages/news.php
@@ -1,4 +1,21 @@
-<?php $aoDatas = $oPdo->query('SELECT * FROM `irpg_news` ORDER BY id DESC')->fetchAll(PDO::FETCH_OBJ); foreach ($aoDatas as $oData) { ?>
-<div class="title">&#8250; <?php echo $oData->title; ?><span class="menuright"><?php echo date('j F Y',$oData->datetime); ?></span></div>
-<div class="contents"><?php echo $oData->contents; ?></div>
-<br /><?php } ?>
+<?php
+if (!isset($_GET['q'])) { header("Location: ../../index.php?q=news"); }
+if (!isset($_GET['p'])) { $_GET['p'] = 1; }
+$iPage = (int)$_GET['p'];
+$iNb = 5;
+$oInstance = dbInstance::get('phpirpg');
+$oIrpgNews = new dbIrpgNews();
+$aoIrpgNews = $oIrpgNews->select('SQL_CALC_FOUND_ROWS irpg_news.*')->order('id DESC')->limit(($iPage-1) * $iNb.','.$iNb)->fetchAll();
+$iIrpgNews = (int)$oInstance->query('SELECT FOUND_ROWS() AS nb')->fetch(PDO::FETCH_OBJ)->nb;
+$aPagination = utils::getPages($iPage, ceil($iIrpgNews / $iNb), 5);
+if ($iPage > ceil($iIrpgNews / $iNb)) { include_once('404.php'); }
+foreach ($aoIrpgNews as $oIrpgNew)  { 
+?><div class="title">&#8250; <?php echo $oIrpgNew->title; ?><span class="date"><?php $date = date_create($oIrpgNew->date_creation); echo date_format($date, 'j F Y'); ?></span></div>
+<div class="contents"><?php echo $oIrpgNew->contents; ?></div><br />
+<?php }
+echo "<div class='pagination'><strong>&#8250; P</strong>age".((ceil($iIrpgNews / $iNb) > 1) ? 's' : '').": ";
+foreach ($aPagination as $iPagination) {
+if ($iPagination == $iPage) echo '<strong>'.$iPage.'</strong> ';
+else echo '<a href="?p='.$iPagination.'">'.$iPagination.'</a> ';
+}
+?><strong>&#8249;</strong></div>
