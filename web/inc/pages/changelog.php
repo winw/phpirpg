df51e41b4b52efa2b973f8f86e4a06b658c539d6
Chris <z3ckz@hotmail.com>
2013-03-01 19:12:36 +0100

    Remplacement de la page dev.php

---
+++ web/inc/pages/changelog.php
@@ -0,0 +1,10 @@
+<?php
+if (!isset($_GET['q'])) { header("Location: ../../index.php?q=changelog"); }
+?>
+<div class="contents">This is the changelog for the #idle-rpg bot, developed by his majesty, <a href="mailto:&#119;&#105;&#110;&#064;&#119;&#097;&#114;&#114;&#105;&#111;&#114;&#104;&#111;&#117;&#115;&#101;&#046;&#110;&#101;&#116;" class="bold">win</a>. If you find any bugs, thank you to report him.<br /><br />You can download the latest version <a href="#" class="bold">here</a>.</div><br />
+<div class="title">&#8250; V 1.2 - Released on 10 February 2013.</div>
+<div class="contents"><blockquote><p>- Added blabla bla bla bla.<br />- Fixed bug bla bla blouh.</p></blockquote></div>
+<div class="title">&#8250; V 1.1 - Released on 10 February 2013.</div>
+<div class="contents"><blockquote><p>- Added blabla bla bla bla.<br />- Fixed bug bla bla blouh.</p></blockquote></div>
+<div class="title">&#8250; V 1.0 - Released on 10 February 2013.</div>
+<div class="contents"><blockquote><p>- First official launch.</p></blockquote></div>
