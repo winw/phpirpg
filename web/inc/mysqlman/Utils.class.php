71b3fadacc5991a49de20694457d9da0b7a5edbc
Chris <z3ckz@hotmail.com>
2013-03-01 19:05:19 +0100

    Ajout fonction number pour differencier les milliers et hte -> htmlentities

--- web/inc/mysqlman/Utils.class.php
+++ web/inc/mysqlman/Utils.class.php
@@ -1,5 +1,14 @@
 <?php
  class Utils {
+  public static $sCharset = 'utf-8';
+  public static $iCompat = ENT_QUOTES;
+  
+  public static function number($number)
+  {
+    $number = number_format($number, 0, ',', ' ');
+    return str_replace(" ", "&nbsp;", $number);
+  }
+  
   public static function duration($iSecs) {
    $aTab = array(
     'year' => 31536000,
@@ -26,5 +35,28 @@
    
    return rtrim($sReturn, ', ');
   }
+  
+  public static function hte($sText, $sCharset = false) {
+  if ($sCharset === false) $sCharset = self::$sCharset;
+  return htmlentities($sText, self::$iCompat, $sCharset);
+  }
+  
+  public static function getPages($iNow, $iResults, $iNums = 5) {
+   $iMoit = floor($iNums/2);
+   $iDebut = ($iNow-$iMoit);
+   $iFin = ($iNow+$iMoit);
+   $aRet = array();
+   if ($iFin > $iResults) {
+    $iFin = $iResults;
+    $iDebut -= $iMoit;
+   }
+   if ($iDebut < 1) {
+    $iDebut = $iNow = 1;
+    $iFin += $iMoit;
+   }
+   
+   return range($iDebut, min($iFin, $iResults));
+  }
+  
  }
 ?>
