deb052f640f3622867202cca03c8eb46431e3120
Chris <z3ckz@hotmail.com>
2013-03-03 00:25:45 +0100

    Inadvertance..

--- web/inc/pages/404.php
+++ web/inc/pages/404.php
@@ -1,4 +1,4 @@
 <?php 
-if ((!isset($_GET['q'])) && (!isset($_GET['q']))) { header("Location:../../index.php"); }
+if (!isset($_GET['q'])) { header("Location:../../index.php"); }
 else { echo "<i>- Error 404: the page you requested does not exist or is not available anymore. You will be redirect in few seconds...</i>"; header("refresh:7; url=index.php"); die(); }
 ?>
